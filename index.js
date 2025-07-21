ef355508d2d7ccb5370621fe3a92bf377054aaab
dysn <59966941+xdysn@users.noreply.github.com>
2025-07-21 13:14:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
 
   // Write your prompt here
   const prompt =
-    "generate a web development content, tips and tricks or something new or some rant or some advice as a tweet, it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis";
+    "find me a shitpost meme image for my x profile, find some memes on x and give me most recent one";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
